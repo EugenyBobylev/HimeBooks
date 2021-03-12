36e5e712680f22c4cb6da0eadbdb7c3c65c52ffd
bobylev <pythoncoder@mail.ru>
2021-03-12 16:33:38 +0200

    added bootstrap to template

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,10 @@
 from flask import render_template
-from app import app
+from app import app, db
+from app.models import Book
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html')
+    books = Book.query.all()
+    return render_template('index.html', books=books)

bf2a98fbf22d7d3ace87185c599f99d0ffd395cc
bobylev <pythoncoder@mail.ru>
2021-03-14 08:19:16 +0200

    deleted unused modules and correct routes and __init__

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,15 @@
-from flask import render_template
+from flask import render_template, request
 from app import app
 from app.models import Book, PdfBook
+
+ROWS_PER_PAGE = 5
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    db_books = Book.query.all()
+    page = request.args.get('page', 1, type=int)
+    db_books = Book.query.paginate(page=page, per_page=ROWS_PER_PAGE).items
+    # db_books = Book.query.all()
     books = [PdfBook(book) for book in db_books]
     return render_template('index.html', books=books)

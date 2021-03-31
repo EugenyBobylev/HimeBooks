5970ce1152a832e0b68255ec3fdf8919fb3c501c
bobylev <pythoncoder@mail.ru>
2021-03-31 16:18:29 +0300

    проделана настройка для запуска через gunicorn

--- config.py
+++ config.py
@@ -6,5 +6,4 @@ basedir = Path(__file__).parent
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'p140d$5qvd#z3vkdcpz))jj#dtwl1$+4^p$7o(sk*6oo17_6pa'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
     BASE_DIR = basedir
